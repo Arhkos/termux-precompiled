d39d736c8ab5bda9a41c47358071239271cc5c2f
Arhkos <87340721+Arhkos@users.noreply.github.com>
2024-02-15 11:45:11 +0100

    Update upgrademiner.sh

--- upgrademiner.sh
+++ upgrademiner.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PS3="Choix de la version: "
 select CHOIX in main x1-a78-a55 em5-a76-a55 em4-a75-a55 em3-a55 a78 a78c a78-a55 a77 a77-a55 a76 a76ae a76-a55 a75 a75-a55 a73 a72 a65 a65ae a57 a55 a53 a35 a34
 do
