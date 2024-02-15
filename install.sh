f3d183f6650636f8272060b4bd6ff8fc4f764b8d
Arhkos <87340721+Arhkos@users.noreply.github.com>
2024-02-15 15:45:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@
 #env ASSUME_ALWAYS_YES=yes pkg install libjansson openssh wget nano -y
 pkg install libjansson openssh wget nano screen jq -y
 
-cat << FIN > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAun5xGZ+hOoN8kUYrmaHuMoMLbKkU/s63xHjnJYOz9R9IiC80bSaIMh5EUkwqUXfTg/4UjAIZ1lXOkMzBf8bZljuC2mUga5CIBVogjc3Zvc/9W7n2pKkxh1PPJs2pnPxOK/LcCQEWbPRhXVTc1CJAuIPXD3ADG6TVO4B/eTM4mprs22cDGkSu2MZhppPRDZF1R6KkaoPTBJeu5S+8q8RZtZUFYmjKkx+h5lOYNbZwVfZPEKA+oSouXaui0jlllqZqKgAYMLemU9rKWL9Jx7JeZ8g/5qZvST0ad57af7+vlMnxBiBNRBdhCMHMOptXVMVyh+ohh8PMv5myzB/bvsk3bQ==
-FIN
-
 sshd
 
 USER=$(whoami)
