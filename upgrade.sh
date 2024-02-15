d7c5c7d5ea488d9f26187b27fc388ddcb0f558d5
Arhkos <87340721+Arhkos@users.noreply.github.com>
2024-02-15 13:56:46 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -15,15 +15,13 @@ MINER=RQpFtHQ8eTT4Dgpn5Te1cB1Jrp5NE1KvzP
 MINERNAME=$MINER.$1
 
 
-wget https://raw.githubusercontent.com/Arhkos/VerusCliMining/main/config.json -O ~/ccminer/config.json
+wget https://raw.githubusercontent.com/Arhkos/termux-precompiled/main/config.json -O ~/ccminer/config.json
 echo "config.json copied  "
 echo "Update de la config pour $1"
 tmpfile=$(mktemp); cp ~/ccminer/config.json "$tmpfile" && jq --arg newval "$MINERNAME" '.user |= $newval' "$tmpfile" >~/ccminer/config.json && rm -f "$tmpfile"
 chmod +x ~/g.sh
 
-echo "pkill screen
-screen -wipe
-screen -dmS miner -L ccminer/start.sh" > ~/g.sh
+wget https://raw.githubusercontent.com/Arhkos/termux-precompiled/main/g.sh -O ~/g.sh
 
 bash ~/g.sh
 
